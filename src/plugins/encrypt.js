30808bb6ee184c1b3da3b36c98011c5507751c7d
NaiboWang-Alienware <naibowang@u.nus.edu>
2021-12-10 16:19:41 +0800

    New rsa key and development address

--- src/plugins/encrypt.js
+++ src/plugins/encrypt.js
@@ -3,10 +3,10 @@ import service from "@/plugins/axios";
 // 实例化一个JSEncrypt对象
 const jse = new JsEncrypt();
 jse.setPublicKey(`-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDP5daFehYHWPXLY0IlwojIh+ld
-zYVn2JtfaThPR0YwY/lhHUtbhiL0Uw3t+CgFYmZ19GMKEkHZS2dZSoogAkD1Rys2
-E8+vNTtJlBt92TOHXzv+Q3LWKfv372LY9i065VEcsybxyItlCGeEYLDTBg8a+iOW
-v8Wj8HHf6RL/vxRcZwIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuKwpNq81z+Ds9mrfpupD47zOO
++4NIqsOGRxSsBBgH5T8OVWeu3myZEzRWOdwjesC1t5gLMnqi5uvi8rCvMepJq6yn
+Owq5Ted5/Cg1HqZN7CPLFXF3wHMxp3pvy3N1dXX1nQj/UMRYYoDcti2S0Zddo5nR
+sFU/u6T5SWn7tfZoCQIDAQAB
 -----END PUBLIC KEY-----`);
 
 export default jse;
